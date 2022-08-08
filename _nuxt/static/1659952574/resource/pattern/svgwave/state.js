5840d5802396b6a195842e2af42343118225bb1a
uiset-com <1246996371@qq.com>
2022-08-08 09:56:33 +0000

    See https://github.com/uiset-com/generate/commit/4f4a3aff5da9ffbcadb0e71f79e7275fc0a98d49 from refs/heads/master

---
+++ _nuxt/static/1659952574/resource/pattern/svgwave/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659952574",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fpattern\u002Fsvgwave",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d19713ee"}}}}(null));
