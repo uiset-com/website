73795274c956416965c036eaf0e60d7be2ade6c1
uiset-com <1246996371@qq.com>
2022-08-09 06:45:14 +0000

    See https://github.com/uiset-com/generate/commit/df21a5260375e552d3b8a0bd9e36048b3469116d from refs/heads/master

---
+++ _nuxt/static/1660027489/resource/avatar/minimalavatars/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660027489",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Favatar\u002Fminimalavatars",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"064cfa19"}}}}(null));
