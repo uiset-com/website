0302fcc1f67d1365688e7ffd7d3e2e00ac10ebdd
uiset-com <1246996371@qq.com>
2022-08-10 05:42:28 +0000

    See https://github.com/uiset-com/generate/commit/f76ea0af3358daddc7bd536a545948360990963f from refs/heads/master

--- _nuxt/static/1660106605/resource/icon/cryptocurrency-icons/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660106605",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Ficon\u002Fcryptocurrency-icons",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"acf7751a"}}}}(null));
