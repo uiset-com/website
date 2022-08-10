2310101a4e556b130b8e2d121b02a107143b40a2
uiset-com <1246996371@qq.com>
2022-08-10 04:43:49 +0000

    See https://github.com/uiset-com/generate/commit/e93b2b1994e393c2d1723b60453f22f63500731a from refs/heads/master

---
+++ _nuxt/static/1660106605/resource/pattern/blobmaker/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660106605",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fpattern\u002Fblobmaker",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"acf7751a"}}}}(null));
