791a4421bff3c79b088252fca1cf92fa576bb6fb
uiset-com <1246996371@qq.com>
2022-08-11 04:12:16 +0000

    See https://github.com/uiset-com/generate/commit/8009bb6a569bd45e3a6388c255543246886d5e64 from refs/heads/master

--- _nuxt/static/1660189954/resource/color/huemint/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660189954",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fcolor\u002Fhuemint",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7c07ede7"}}}}(null));
