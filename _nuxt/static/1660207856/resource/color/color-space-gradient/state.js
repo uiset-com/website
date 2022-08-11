95f6d31278e38bed1804f331d96f689d55c348a5
uiset-com <1246996371@qq.com>
2022-08-11 08:51:30 +0000

    See https://github.com/uiset-com/generate/commit/bd8d1fb915448e5f1d91dcbef22ac29c3874c204 from refs/heads/master

---
+++ _nuxt/static/1660207856/resource/color/color-space-gradient/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660207856",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fcolor\u002Fcolor-space-gradient",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d63e5539"}}}}(null));
