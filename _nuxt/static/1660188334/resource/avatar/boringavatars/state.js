17a6c9df70333513703ccca81f55ed662046a42b
uiset-com <1246996371@qq.com>
2022-08-11 03:25:59 +0000

    See https://github.com/uiset-com/generate/commit/8009bb6a569bd45e3a6388c255543246886d5e64 from refs/heads/master

---
+++ _nuxt/static/1660188334/resource/avatar/boringavatars/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660188334",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Favatar\u002Fboringavatars",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b47c4e3c"}}}}(null));
