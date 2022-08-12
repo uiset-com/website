da12f920040e0468dba1cd728c78453ae1629c7a
uiset-com <1246996371@qq.com>
2022-08-12 04:16:13 +0000

    See https://github.com/uiset-com/generate/commit/ba4dfdc93336d35d9667801557f3d31def5bf462 from refs/heads/master

--- _nuxt/static/1660276750/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660276750",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a39f67d6"}}}}(null,"\u002F"));
