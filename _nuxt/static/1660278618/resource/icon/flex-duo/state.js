b372b428e3f74fc1f81feacc8e6b7b8b4062c5a4
uiset-com <1246996371@qq.com>
2022-08-12 04:30:52 +0000

    See https://github.com/uiset-com/generate/commit/ac4761edad2486d70d0c0a0ffcbc99db901b37d7 from refs/heads/master

---
+++ _nuxt/static/1660278618/resource/icon/flex-duo/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660278618",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Ficon\u002Fflex-duo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"22779a2d"}}}}(null));
