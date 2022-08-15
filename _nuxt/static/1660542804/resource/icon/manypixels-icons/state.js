edaf7443a8dd3e8b29f5a5779dd76def13a82c49
uiset-com <1246996371@qq.com>
2022-08-15 05:53:53 +0000

    See https://github.com/uiset-com/generate/commit/70205c07a6f8c0525fbfe43f656242ae6ef297ec from refs/heads/master

---
+++ _nuxt/static/1660542804/resource/icon/manypixels-icons/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660542804",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Ficon\u002Fmanypixels-icons",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0b5a0429"}}}}(null));
