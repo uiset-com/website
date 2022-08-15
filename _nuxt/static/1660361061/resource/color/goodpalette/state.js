5a2a7b16cef069fae86b35dcb9d6d3a9f228bdca
uiset-com <1246996371@qq.com>
2022-08-15 02:55:41 +0000

    See https://github.com/uiset-com/generate/commit/70205c07a6f8c0525fbfe43f656242ae6ef297ec from refs/heads/master

--- _nuxt/static/1660361061/resource/color/goodpalette/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660361061",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fcolor\u002Fgoodpalette",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a9ac42a2"}}}}(null));
---
+++ _nuxt/static/1660532112/resource/icon/eva-icons/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660532112",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Ficon\u002Feva-icons",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e58c43ca"}}}}(null));
