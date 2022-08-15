ad6f67fb232da7bc22c98d167afa678c800ca9e3
uiset-com <1246996371@qq.com>
2022-08-15 07:47:30 +0000

    See https://github.com/uiset-com/generate/commit/70205c07a6f8c0525fbfe43f656242ae6ef297ec from refs/heads/master

--- _nuxt/static/1660543806/resource/icon/flag-icons/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660543806",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Ficon\u002Fflag-icons",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3926b2dc"}}}}(null));
