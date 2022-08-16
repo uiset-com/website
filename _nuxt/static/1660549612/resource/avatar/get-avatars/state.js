b0049fe26fcdbbe52a947d699688ae64289b7b29
uiset-com <1246996371@qq.com>
2022-08-16 16:06:11 +0000

    See https://github.com/uiset-com/generate/commit/70205c07a6f8c0525fbfe43f656242ae6ef297ec from refs/heads/master

--- _nuxt/static/1660549612/resource/avatar/get-avatars/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660549612",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Favatar\u002Fget-avatars",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e3784b47"}}}}(null));
