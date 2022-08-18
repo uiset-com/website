d7970976bd64982e5cf107ff87c67575df7c6820
uiset-com <1246996371@qq.com>
2022-08-18 08:25:20 +0000

    See https://github.com/uiset-com/generate/commit/465133828c96e71e621033d12302cc3b646c1fef from refs/heads/master

--- _nuxt/static/1660706417/resource/pattern/coolbackgrounds/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660706417",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fpattern\u002Fcoolbackgrounds",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b88cb597"}}}}(null));
