bd76209ff4dd79e0dc4bbd844b00b60418e355fa
uiset-com <1246996371@qq.com>
2022-08-26 05:20:32 +0000

    See https://github.com/uiset-com/generate/commit/6195e96fa2b9420f4dcf5c959bd7f5c698549067 from refs/heads/master

--- _nuxt/static/1661165351/resource/beatify/snippet-shot/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661165351",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fbeatify\u002Fsnippet-shot",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4867edda"}}}}(null));
