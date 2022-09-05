aca66e7eb9f18d35b9bec90e81cba96986dfaab0
uiset-com <1246996371@qq.com>
2022-09-05 04:01:39 +0000

    See https://github.com/uiset-com/generate/commit/6195e96fa2b9420f4dcf5c959bd7f5c698549067 from refs/heads/master

--- _nuxt/static/1662348882/resource/color/hypercolor/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662348882",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fcolor\u002Fhypercolor",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ec4a45ef"}}}}(null));
