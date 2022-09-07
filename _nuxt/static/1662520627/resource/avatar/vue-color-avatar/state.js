000cc52ec67b544b145012abb4c5341315a7be9a
uiset-com <1246996371@qq.com>
2022-09-07 03:21:59 +0000

    See https://github.com/uiset-com/generate/commit/fa88747eca81479b792d90ad26109b4e54e0be70 from refs/heads/master

--- _nuxt/static/1662520627/resource/avatar/vue-color-avatar/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662520627",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Favatar\u002Fvue-color-avatar",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1e61c2b0"}}}}(null));
