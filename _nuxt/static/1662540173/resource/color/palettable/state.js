5b80a85193f4170f80e3611445bcd1dc7962ada3
uiset-com <1246996371@qq.com>
2022-09-07 08:43:19 +0000

    See https://github.com/uiset-com/generate/commit/9d7469446b774f4e77e01c469ed6bc8bdd234a99 from refs/heads/master

---
+++ _nuxt/static/1662540173/resource/color/palettable/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662540173",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fcolor\u002Fpalettable",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"86d2b009"}}}}(null));
