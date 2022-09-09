d30af6fb7f315426017c823509110b023cd606f4
uiset-com <1246996371@qq.com>
2022-09-09 10:04:38 +0000

    See https://github.com/uiset-com/generate/commit/9d7469446b774f4e77e01c469ed6bc8bdd234a99 from refs/heads/master

--- _nuxt/static/1662540173/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662540173",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"86d2b009"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1662717851/resource/color/brandcolors/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662717851",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fcolor\u002Fbrandcolors",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dd34cd33"}}}}(null));
