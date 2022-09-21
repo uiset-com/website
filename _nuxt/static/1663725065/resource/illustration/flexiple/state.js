086353987117d16f535eab4c9d2fa93bf280dbb4
uiset-com <1246996371@qq.com>
2022-09-21 01:51:34 +0000

    See https://github.com/uiset-com/generate/commit/9d7469446b774f4e77e01c469ed6bc8bdd234a99 from refs/heads/master

---
+++ _nuxt/static/1663725065/resource/illustration/flexiple/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663725065",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresource\u002Fillustration\u002Fflexiple",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"50f057eb"}}}}(null));
